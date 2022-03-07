27d2c2ed69ae7ad2cf3a317bb071c1d6132cfef5
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-07 16:08:54 +0530

    NOT INTEGRATED YET

---
+++ user-panel/main/gulpfile.js
@@ -0,0 +1,20 @@
+
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const files = {
+    scssPath: 'src/scss/**/*.scss',
+    cssPath: 'src/dist/css'
+}
+function scssTask() {
+    return src(files.scssPath, { sourcemaps: true })
+        .pipe(sass()) 
+        .pipe(postcss([autoprefixer(), cssnano()])) 
+        .pipe(dest(files.cssPath, { sourcemaps: '.' }));
+}
+function watchTask() {
+    watch([files.scssPath], parallel(scssTask));
+}
+exports.default = series(scssTask, watchTask);
