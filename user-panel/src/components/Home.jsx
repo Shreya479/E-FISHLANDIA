4152719b95ad2a50050523756c044e3f5bb19be7
Shreya Singh <72337463+Shreya479@users.noreply.github.com>
2022-03-08 11:20:46 +0530

    Update Home.jsx

--- user-panel/src/components/Home.jsx
+++ user-panel/src/components/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https://www.highdefwallpaper.com/wp-content/uploads/2016/09/animated-aquarium-fish.jpg" alt="first" class="d-block w-100" height={670} />
+            <img src="https://cdn.mos.cms.futurecdn.net/RY2EpSo74hvYXyAVpTN2Gg-1200-80.jpg" alt="first" class="d-block w-100" height={670} />
           </div>
           <div class="carousel-item">
-            <img src="https://th.bing.com/th/id/R.43263042e8891078e68a08c0b743e39e?rik=ipuDfeiO6H6LVg&riu=http%3a%2f%2ffc01.deviantart.net%2ffs71%2ff%2f2011%2f129%2f7%2fa%2ffish_animation_thingy_by_tranet-d3fyw89.gif&ehk=yRyJIB%2bGOu1zuM5Rz%2bodHu20weSU%2bmws01EUT%2fyQQnk%3d&risl=&pid=ImgRaw&r=0" alt="first" class="d-block w-100" height={670} />
+            <img src="https://getwallpapers.com/wallpaper/full/e/e/d/646796.jpg" alt="first" class="d-block w-100" height={670} />
           </div>
           <div class="carousel-item">
-            <img src="https://3.bp.blogspot.com/-PoytWsNjL1M/Uai_JsC6LJI/AAAAAAAACdg/xSJns4AS1DE/s1600/Animated%2BFish%2BWallpaper-778219.jpg" alt="first" class="d-block w-100" height={670} />
+            <img src="https://fishsubsidy.org/wp-content/uploads/2020/02/fish-tank.jpg" alt="first" class="d-block w-100" height={670} />
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
