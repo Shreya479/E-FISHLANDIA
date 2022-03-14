7602a025684d7f65957410524ed0d36e85e84033
samiran2076 <77327194+samiran2076@users.noreply.github.com>
2022-03-14 20:28:42 +0530

    Add files via upload

---
+++ User-panel (with integration)/backend/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.review.reviewsystem.repository;
+
+import com.review.reviewsystem.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review,Integer> {
+}
