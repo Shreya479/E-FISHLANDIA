dee249fb2cedf6c18d1bacf9f026416116492f27
samiran2076 <77327194+samiran2076@users.noreply.github.com>
2022-03-14 17:36:15 +0530

    Add files via upload

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,24 @@
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
+import React, { useEffect, useState } from 'react';
+
 
 export default function Home() {
+
+ const [user,setUser] = useState([]);
+ const[name,setName]=useState('')
+ const[students,setStudents]=useState([])
+
+ useEffect(()=>{
+  fetch("http://localhost:8080/helpdesk/getAll")
+  .then(res=>res.json())
+  .then((result)=>{
+    setStudents(result);
+  }
+)
+},[])
+
   return (
     <div className="home">      
       <div className="main__title">
@@ -12,6 +28,19 @@ export default function Home() {
           </div>
         </div>
       <FeaturedInfo /> 
+      <div className="problems">
+        <h4>PROBLEMS</h4>
+        {students.map(student=>(
+            <p style={{margin:"10px",padding:"1px", textAlign:"left"}} key={student.id}>
+             Sl.No: {student.id} &nbsp;
+             Name: {student.name} &nbsp;&nbsp;
+             Problem: {student.problem} &nbsp;
+             
+            </p>
+          ))
+          }
+      </div>
+
     </div>
   );
 }
